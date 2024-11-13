76435c8f37deb896a14dcc550e1b73193fbf1837
Radishevskiy <patrikstar16724@gmail.com>
2024-11-13 20:43:57 +0300

    вапролывапорлр

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Student28\Desktop\exe_git\Experimental\.venv
+set VIRTUAL_ENV=C:\Users\17vip\Desktop\git_rad\Experimental\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
